30e0aac0ad115e6fde11c06e81cf410214a38491
chelm <christopher_helm@esri.com>
2013-07-22 09:27:20 -0600

    port 80 for heroku

--- config/application.js
+++ config/application.js
@@ -4,7 +4,7 @@ module.exports = {
 	appName: "Sails Application",
 
 	// Port this Sails application will live on
-	port: 1337,
+	port: 80,
 
 	// The environment the app is deployed in 
 	// (`development` or `production`)
