463023301e4d7d8c4ec953823402628c0adacd95
chelm <christopher.helm@gmail.com>
2013-08-18 21:01:25 -0600

    clean up views

--- api/providers/gist/controller/index.js
+++ api/providers/gist/controller/index.js
@@ -6,35 +6,35 @@ module.exports = {
   provider: true,
  
   index: function(req, res){
-    res.send('GIST INDEX');
+    res.render('gist/index');
   },
 
   findOne: function(req, res){
-      function send( err, data ){
-          if ( err ){
-            res.json( err, 500 );
-          } else { 
-            if ( data ){
-              res.json( data );
-            } else {
-              res.send('There a problem accessing this gist', 500);
-            }
+    function send( err, data ){
+        if ( err ){
+          res.json( err, 500 );
+        } else { 
+          if ( data ){
+            res.json( data );
+          } else {
+            res.send('There a problem accessing this gist', 500);
           }
-      };
-      if ( req.params.id ){
-        var id = req.params.id;
-        if ( !Cache.gist[ id ] ){
-          Geohub.gist( {id: id }, function( err, data ){
-            Cache.gist[ id ] = JSON.stringify( data[0] ); 
-            send( err, data[0]);
-          });  
-        } else {
-          send( null, JSON.parse( Cache.gist[ id ] ));
         }
-        
+    };
+    if ( req.params.id ){
+      var id = req.params.id;
+      if ( !Cache.gist[ id ] ){
+        Geohub.gist( {id: id }, function( err, data ){
+          Cache.gist[ id ] = JSON.stringify( data[0] ); 
+          send( err, data[0]);
+        });  
       } else {
-        res.send('Must specify a user and gist id', 404);
+        send( null, JSON.parse( Cache.gist[ id ] ));
       }
+      
+    } else {
+      res.send('Must specify a user and gist id', 404);
+    }
   },
 
   featureservice: function(req, res){
