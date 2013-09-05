884f81e2ff11c442d577de8a3ad47248b621ebb1
chelm <christopher.helm@gmail.com>
2013-09-05 16:06:21 -0600

    added redis caching

--- packages/koop-core/api/models/Cache.js
+++ packages/koop-core/api/models/Cache.js
@@ -5,56 +5,73 @@ module.exports = {
   checkTime: (60*60*1000), // 60 mins
 
   insert: function( type, key, data, callback ){
-    if ( !this[ type ] ){
-      this[ type ] = {};
-    }
-
-    this[ type ][ key ] = {
+    var json = {
       data: data,
       timer: new Date().getTime() + this.checkTime
     };
+
+    //console.log('REDIS set', Cache.redis);
+    if ( Cache.redis ){
+      Cache.redis.hset(type, key, JSON.stringify({
+        data: data,
+        timer: new Date().getTime() + this.checkTime
+      }));
+    } else {
+      if ( !this[ type ] ){
+        this[ type ] = {};
+      }
+      this[ type ][ key ] = json;
+    }
     callback(null, true);
+  },
+
+  process: function( entry, callback ){
+    var now = new Date();
+    if ( !entry ){
+      callback( 'Not found', null);
+    } else {
+      // checks the timer 
+      if ( now > entry.timer ){
+
+        var _internalCB = function(err, success){
+            if ( !success ){
+              // cache returned true, return current data
+              callback( null, entry.data );
+            } else {
+              self.insert(type, key, success.data, function(err, res){
+                callback( err, success.data );
+              });
+            }
+            self.resetTimer( type, key );
+        };
+
+        // expired, hit the API to check the latest sha
+        if ( global[type].checkCache ) {
+          global[type].checkCache( key, entry.data, _internalCB);
+        } else {
+          callback( null, entry.data ); }
+      } else {
+        // return the data 
+        callback( null, entry.data );
+      }
+    }
   },
 
   get: function(type, key, callback ){
     var self = this;
 
-    if ( this[ type ] ){
+    //if ( this[ type ] ){
 
-      var entry = this[ type ][ key ],
-        now = new Date();
-
-      if ( !entry ){
-        callback( 'Not found', null);
+      if ( Cache.redis ){
+        Cache.redis.hget(type, key, function(err, result){
+          self.process( JSON.parse( result ), callback );     
+        });
       } else { 
-        // checks the timer 
-        if ( now > entry.timer ){
-
-          var _internalCB = function(err, success){
-              if ( !success ){
-                // cache returned true, return current data
-                callback( null, entry.data );
-              } else {
-                self.insert(type, key, success.data, function(err, res){
-                  callback( err, success.data );
-                });
-              }
-              self.resetTimer( type, key ); 
-          };
-          
-          // expired, hit the API to check the latest sha
-          if ( global[type].checkCache ) {
-            global[type].checkCache( key, entry.data, _internalCB);
-          } else {
-            callback( null, entry.data ); }
-        } else {
-          // return the data 
-          callback( null, entry.data );
-        }
+        self.process( this[ type ][ key ], callback );
       }
-    } else {
-      callback( 'Not found', null);
-    }
+    //} else {
+    //  callback( 'Not found', null);
+    //}
 
   },
 
