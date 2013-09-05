cdabfb33f5336629938d9f0496a1544a65e05967
chelm <christopher.helm@gmail.com>
2013-09-05 16:06:21 -0600

    added redis caching

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -2,8 +2,10 @@
 // Code to run before launching the app
 //
 // Make sure you call cb() when you're finished.
+var redis = require("redis");
+
 module.exports.bootstrap = function (cb) {
   setTimeout(function(){ require('../api/providers/'); }, 1000);
-  //Cache.start();
+  Cache.redis = redis.createClient();
 	cb();
 };
