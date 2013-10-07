974a8b86139611e6aa3ae74f00937a09c32389a1
chelm <christopher.helm@gmail.com>
2013-10-07 11:27:07 -0600

    adding mongo client

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -6,11 +6,15 @@ var argv = require('optimist').argv;
 
 module.exports.bootstrap = function (cb) {
   setTimeout(function(){ require('../api/providers/'); }, 1000);
-  if (argv.redis){
+  /*if (argv.redis){
     console.log('Storage: Using Redis Storage...');
     var redis = require("redis");
     Cache.redis = redis.createClient();
-    
+  } else*/ 
+  if ( argv.mongo ){
+    console.log('Storage: Using Mongo Storage...');
+    var mongoskin = require("mongoskin");
+    Cache.db = mongoskin.db('localhost:27017/koop?auto_reconnect=true&poolSize=10');
   }
 	cb();
 };
