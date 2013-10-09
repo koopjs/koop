62c2e1eb43e2eb1f8ed6faec8e10db1cab569bff
chelm <christopher.helm@gmail.com>
2013-10-09 13:40:00 -0600

    added fix for layered services

--- api/models/Cache.js
+++ api/models/Cache.js
@@ -19,11 +19,12 @@ module.exports = {
       Cache.redis.get( type+':'+key+':timer', function( error, timer){
         if ( timer ){
           // got a timer, therefore we are good and just return
+          console.log('Cache timer exists, not checking for updates', key);
           callback( null, data );
         } else {
-
           // expired, hit the API to check the latest sha
           if ( global[type] && global[type].checkCache ) {
+            console.log('No cache timer, checking service for updates') 
             global[type].checkCache( key, data, function(err, success){
               if ( !success ){
                 // false is good -> reset timer and return data
@@ -34,6 +35,7 @@ module.exports = {
                 });
               } else {
                 // we need to remove and save new data 
+                console.log('Setting a timer', key);
                 self.remove(type, key, function(){
                   self.insert(type, key, success, function(err, res){
                     Cache.redis.set( type+':'+key+':timer', key, function( error, timer){
@@ -45,6 +47,7 @@ module.exports = {
               } 
             });
           } else {
+            console.log('Provider has no checkCache method, sending cached data');
             callback( null, data ); 
           }
         }
@@ -60,31 +63,9 @@ module.exports = {
 
   get: function(type, key, callback ){
     var self = this;
-
-      //if ( Cache.db ){
-
-        // if redis has a key, then we select and return
-        //Cache.redis.get( type+':' + key + ':info', function( error, check){
-          //if ( check ) {
-            Cache.db.select( type+':'+key, function(err, result){
-              self.process( type, key, result, callback );     
-            });
-          //} else {
-          //  Cache.redis.set( type+':'+key + ':info', key, function( error, check){
-          //    Cache.redis.expires(type+':'+key + ':info', 60*60);
-             
-          //  });
-          //}  
-        //}); 
- 
-      /*} else { 
-        if ( this[ type ] ){
-          self.process( type, key, this[ type ][ key ], callback );
-        } else {
-          callback( 'Not found', null);
-        }
-      }*/
-
+    Cache.db.select( type+':'+key, function(err, result){
+      self.process( type, key, result, callback );     
+    });
   },
 
   resetTimer: function( type, key ){
