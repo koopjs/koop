36f46766adea3172314b1861e95011a8a1ba7d84
chelm <christopher.helm@gmail.com>
2014-02-17 12:19:47 -0700

    rm files

--- packages/koop-core/test/helpers/Cache.js
+++
@@ -1,6 +0,0 @@
-var PostGIS = require('../../api/models/PostGIS.js'),
-  Cache = require('../../api/models/Cache.js');
-
-Cache.db = PostGIS.connect( 'postgres://localhost/koopdev' );
-
-module.exports = Cache;
