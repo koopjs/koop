f437034c38f6be9c01008b312c3702af5139ba83
chelm <christopher.helm@gmail.com>
2014-02-18 19:32:04 -0700

    fixing field types

--- api/models/FeatureServices.js
+++ api/models/FeatureServices.js
@@ -15,11 +15,15 @@ module.exports = {
   },
 
   fieldType: function( value ){
-    var type = typeof( value );
-    if ( type == 'number'){
-      type = ( this.isInt( value ) ) ? 'integer' : 'float';
+    if ( parseInt(value) ){
+      return this.fieldTypes[ 'integer' ];
+    } else { 
+      var type = typeof( value );
+      if ( type == 'number'){
+        type = ( this.isInt( value ) ) ? 'integer' : 'float';
+      }
+      return this.fieldTypes[ type ];
     }
-    return this.fieldTypes[ type ];
   },
 
   // is the value an integer?
