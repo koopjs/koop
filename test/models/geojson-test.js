1f8e3a60e43b6c25c739a7f81cdfd7cda94f57f0
chelm <christopher.helm@gmail.com>
2014-02-25 08:45:35 -0700

    fixes for new terraformer

--- test/models/geojson-test.js
+++ test/models/geojson-test.js
@@ -2,22 +2,23 @@ var should = require('should');
 
 before(function (done) {
   fs = require('../../api/models/FeatureServices.js');
-//  data = require('../fixtures/ski_areas.json');
+  esri_json = require('../fixtures/ski.geojson');
   GeoJSON = require('../../api/models/GeoJSON.js');
   done();
 });
 
 describe('GeoJSON Model', function(){
 
-  /*  describe('when converting esri style features to geojson', function(){
+    describe('when converting esri style features to geojson', function(){
       it('should return a proper geojson object', function(done){
-        GeoJSON.fromEsri( data.features, function( err, geojson ){
+        GeoJSON.fromEsri( esri_json, function( err, geojson ){
           geojson.should.be.an.instanceOf(Object);
+          geojson.features.length.should.equal(esri_json.features.length);
           done();
         });
       });
     });
-  */
+  
 
 });
 
