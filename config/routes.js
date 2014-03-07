def5f77f8127cd187c4a353938a9630f3ad4fe12
chelm <christopher.helm@gmail.com>
2014-03-06 20:11:54 -0700

    remove req logging

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,11 @@
 module.exports.routes = {
 
-  '/*': function(req, res, next) {sails.log.verbose(req.method, req.url); next();},
-	
-	'/' : {
-		controller	: 'home',
+  //'/*': function(req, res, next) {sails.log.verbose(req.method, req.url); next();},
+
+  '/' : {
+    controller	: 'home',
     action: 'index'
-	},
+  },
 
   'get /services': {
     controller: 'services',
