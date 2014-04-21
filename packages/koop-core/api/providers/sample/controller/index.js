20e3e5971cd17da534a9e4bea79e8b76ad5a8147
chelm <christopher.helm@gmail.com>
2014-04-20 21:38:00 -0600

    pairing it all down and moving everything in modules

--- packages/koop-core/api/providers/sample/controller/index.js
+++
@@ -1,5 +0,0 @@
-
-exports.provider = false,
-exports.index = function(req, res){
-  res.send('Sample Providers, to make this a real one set provider true');
-};
--- packages/koop-core/test/helpers/Cache.js
+++
@@ -1,7 +0,0 @@
-var Cache = require('../../api/models/Cache.js'),
-  config = require('../../config/local.js'),
-  PostGIS = require('../../api/models/PostGIS.js');
-
-Cache.db = PostGIS.connect(config.db.postgis.conn);
-
-module.exports = Cache;
