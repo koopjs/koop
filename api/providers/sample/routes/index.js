4c1e8d0c0b822022ac5d3ef29a74aa72dbdbae9e
chelm <christopher.helm@gmail.com>
2014-04-20 21:38:00 -0600

    pairing it all down and moving everything in modules

--- api/providers/sample/routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'get /sample': {
-    controller: 'sample',
-    action: 'index'
-  }
-};
--- server.js
+++ server.js
@@ -6,20 +6,21 @@ process.env.UV_THREADPOOL_SIZE = process.env.UV_THREADPOOL_SIZE || Math.ceil(Mat
 
 var cors = require("cors"),
     express = require("express"),
+    Peechee = require('peechee'),
+    spawnasync = require('spawn-async'),
+    bunyan = require('bunyan'),
     config = require("config"),
     bodyParser = require('body-parser'),
     responseTime = require("response-time"),
-    koop = require('./lib/koop');
-
-var server = require("./lib/app")(config);
+    koop = require('koop-server')(config);
 
 global['config'] = config;
 
 // Scan package for koop plugins and require them . 
-try { server.register(require("koop-github")); } catch (e) {}
-try { server.register(require("koop-gist")); } catch (e) {}
-try { server.register(require("koop-socrata")); } catch (e) {}
-try { server.register(require("koop-agol")); } catch (e) {}
+try { koop.register(require("koop-github")); } catch (e) {}
+try { koop.register(require("koop-gist")); } catch (e) {}
+try { koop.register(require("koop-socrata")); } catch (e) {}
+try { koop.register(require("koop-agol")); } catch (e) {}
 
 var app = express();
 
@@ -37,8 +38,38 @@ if (process.env.NODE_ENV === "development") {
 app.use(bodyParser());
 
 app.use(express.static(__dirname + "/public"));
-app.use(server);
 
-app.listen(process.env.PORT || config.port || 8080, function() {
+// add koop middleware
+app.use(koop);
+
+app.listen(process.env.PORT || config.port,  function() {
   console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+
+  if ( config.db.postgis ){
+    Cache.db = PostGIS.connect( config.db.postgis.conn );
+  } else {
+    Cache.db = Local;
+  }
+
+  //config.defaultStyle = fs.readFileSync( __dirname + '/templates/renderers/style.mss', 'utf8' );
+
+  config.log = new bunyan({
+    'name': 'koop-log',
+    streams: [{
+      type: 'rotating-file',
+      path: config.logfile,
+      period: '1d',   // daily rotation
+      count: 3        // keep 3 back copies
+    }]
+  });
+
+  // allow us to kick off system commands w/o blocking
+  config.worker = spawnasync.createWorker({'log': config.log});
+
+  // set up an instance of peechee for saving files (downloads) 
+  if ( config.peechee && config.peechee.type == 's3' ){
+    global['peechee'] = new Peechee( config.peechee );
+  }
+
 });
+
