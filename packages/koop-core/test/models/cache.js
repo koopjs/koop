89de3f8347e0bcc14c3077cabd7ff714891807c2
chelm <christopher.helm@gmail.com>
2014-12-30 13:51:51 -0600

    adding test

---
+++ packages/koop-core/test/models/cache.js
@@ -0,0 +1,59 @@
+var should = require('should');
+
+before(function (done) {
+  key = 'test:repo:file';
+  repoData = require('../fixtures/repo.geojson');
+  PostGIS = require('../../lib/PostGIS.js');
+  SQLite = require('../../lib/SQLite.js');
+
+  CacheHelper = require('../helpers/Cache.js');
+  Cache = CacheHelper.cache;
+  config = CacheHelper.config;
+
+  done();
+});
+
+describe('Cache Model Tests', function(){
+    describe('when caching a github file', function(){
+
+      afterEach(function(done){
+        Cache.remove('repo', key, {layer: 0}, done);
+      });
+
+      it('should error when missing key is sent', function(done){
+        Cache.get('repo', key+'-BS', {}, function( err, data ){
+          should.exist( err );
+          done();
+        });
+      });
+
+      it('should insert and remove the data', function(done){
+        Cache.insert( 'repo', key, repoData[0], 0, function( error, success ){
+          should.not.exist(error);
+          success.should.equal( true );
+              done();
+        /*  Cache.remove('repo', key, {layer: 0}, function( err, d ){
+            should.not.exist(err);
+            Cache.get('repo', key, {}, function(err, result){
+              should.exist( err );
+            });
+          });
+        */
+        });
+      });
+
+      it('should insert and get the sha', function(done){
+        Cache.insert( 'repo', key, repoData[0], 0, function( error, success ){
+          should.not.exist(error);
+          success.should.equal( true );
+          Cache.get('repo', key, {}, function( err, d ){
+            should.not.exist(err);
+            should.exist(d[0].name);
+            d[0].name.should.equal('snow.geojson');
+            done();
+          });
+        });
+      });
+    });
+
+});
---
+++ packages/koop-core/test/models/mapnik-tiles.test.js
@@ -0,0 +1,75 @@
+var should = require('should');
+
+before(function (done) {
+  data = require('../fixtures/11.geojson');
+  koop = require('../../lib/index');
+  tiles = new require('../../lib/Tiles.js')( koop );
+  done();
+});
+
+describe('Mapnik Tiles Model', function(){
+
+    describe('errors when params are wrong', function(){
+      it('when missing a z', function(done){
+        tiles.get( {x: 1, y: 1, format: 'png', key: 'fake-key'}, {}, function( err, res ){
+          should.exist(err);
+          should.not.exist(res);
+          done();
+        });
+      });
+    });
+
+    describe('vector-tiles', function(){
+      it('when creating a tile', function(done){
+        var file = __dirname + '/../fixtures/co.6.13.24.vector.pbf',
+          format = 'vector.pbf';
+
+        tiles._stash( file, format, data, 6, 12, 24, function( err, res ){
+          should.not.exist(err);
+          should.exist(res);
+          done();
+        });
+      });
+
+       it('when creating a tile', function(done){
+        var file = __dirname + '/../fixtures/5.5.12.vector.pbf',
+          format = 'vector.pbf';
+
+        tiles._stash( file, format, data, 5, 5, 11, function( err, res ){
+          should.not.exist(err);
+          should.exist(res);
+          done();
+        });
+      });
+    });
+
+    describe('png-tiles', function(){
+      it('when creating a tile', function(done){
+
+        var file = __dirname + '/../fixtures/5.5.12.png',
+          format = 'png';
+
+        tiles._stash( file, format, data, 5, 5, 12, function( err, res ){
+          should.not.exist(err);
+          should.exist(res);
+          done();
+        });
+      });
+    });
+
+    describe('utf-tiles', function(){
+      it('when creating a tile', function(done){
+
+        var file = __dirname + '/../fixtures/5.5.12.utf',
+          format = 'utf';
+
+        tiles._stash( file, format, data, 5, 5, 12, function( err, res ){
+          should.not.exist(err);
+          should.exist(res);
+          done();
+        });
+      });
+    });
+
+});
+
