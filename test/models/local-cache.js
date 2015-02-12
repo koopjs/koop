5e0fba17d220f756234f71977bb25e55618d8e5c
chelm <christopher.helm@gmail.com>
2015-02-12 08:38:24 -0600

    localcache is essentially ready to be the default

---
+++ test/models/local-cache.js
@@ -0,0 +1,85 @@
+var should = require('should');
+
+before(function (done) {
+  insertKey = 'test:repo:file';
+  key = 'test:repo:file:0';
+  repoData = require('../fixtures/repo.geojson');
+  repoData[0].info = {name: 'somename'};
+  Cache = require('../../lib/Local.js');
+  done();
+});
+
+describe('Local Cache Tests', function(){
+
+    describe('when requesting bogus data', function(){
+      it('should error when missing key is sent', function(done){
+        Cache.select(key + '-fake', {}, function( err, data ){
+          should.exist( err );
+          done();
+        });
+      });
+    });
+
+    describe('when caching geojson', function(){
+
+      beforeEach(function(done){
+        Cache.insert( insertKey, repoData[0], 0, function( error, success ){
+          done();
+        });
+      });
+
+      afterEach(function(done){
+        Cache.remove(key, done);
+      });
+
+
+      it('should remove the data', function(done){
+          Cache.remove(key, function( err, d ){
+            should.not.exist( err );
+            Cache.select( key, {}, function( err, result ){
+              should.exist( err );
+              done();
+            });
+          });
+      });
+
+      it('should get the name', function(done){
+          Cache.select(key, {}, function( err, d ){
+            should.not.exist(err);
+            should.exist(d[0].name);
+            d[0].name.should.equal('forks.geojson');
+            done();
+          });
+      });
+
+      it('should get count', function(done){
+          Cache.getCount(key, {}, function( err, count ){
+            should.not.exist(err);
+            should.exist(count);
+            count.should.equal(417);
+            done();
+          });
+      });
+
+      it('should get info', function(done){
+          Cache.getInfo(key, function( err, info ){
+            should.not.exist(err);
+            should.exist( info );
+            done();
+          });
+      });
+
+      it('should update info', function(done){
+          Cache.updateInfo( key, {'name': 'newname'}, function( err, success ){
+            should.not.exist( err );
+            Cache.getInfo(key, function( err, info ){
+              should.exist(info);
+              info.name.should.equal('newname');
+              done();
+            });
+          });
+      });
+
+    });
+
+});
