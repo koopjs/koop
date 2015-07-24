0d0ab1bc4b05409ac7a00d39737e0531839d1f04
Christopher Helm <christopher.helm@gmail.com>
2015-07-24 09:41:30 -0600

    Merge pull request #211 from koopjs/no-slashes
    delinting tests and adding basemodel test

--- packages/koop-core/test/models/exporter-test.js
+++ packages/koop-core/test/models/exporter-test.js
@@ -94,8 +94,8 @@ describe('exporter Model', function () {
       var params = exporter.getOgrParams(format, inFile, outFile, null, options).split(' ')
       params[10].should.equal('\'PROJCS["NAD_1983_CSRS_UTM_Zone_21N",GEOGCS["GCS_North_American_1983_CSRS",DATUM["D_North_American_1983_CSRS",SPHEROID["GRS_1980",6378137.0,298.257222101],TOWGS84[-0.9956,1.9013,0.5215,0.025915,0.009426,0.011599,-0.00062]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-57.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]\'')
       // make sure the format is "Esri Shapefile"
-      var format = params[5] +' '+ params[6];
-      format.should.equal('"ESRI Shapefile"')
+      var outFormat = params[5] + ' ' + params[6]
+      outFormat.should.equal('"ESRI Shapefile"')
       done()
     })
 
