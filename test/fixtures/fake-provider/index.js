6f282f267e033451c5ea74a3b377a84bf9a037f3
Nate Goldman <nate@ngoldman.me>
2015-08-20 16:36:45 -0700

    :tractor: add type to provider fixture

--- test/fixtures/fake-provider/index.js
+++ test/fixtures/fake-provider/index.js
@@ -1,5 +1,6 @@
 exports.name = 'test-provider'
 exports.pattern = '/:id'
+exports.type = 'provider'
 exports.controller = require('./controller')
 exports.routes = require('./routes')
 exports.model = require('./models/fake')
