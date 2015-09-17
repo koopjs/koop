8c400e285873fc1062b36a0f83f754194ad6e705
dmfenton <dfenton@esri.com>
2015-09-17 08:41:32 -0400

    WIP

--- test/models/exporter-test.js
+++ test/models/exporter-test.js
@@ -83,10 +83,13 @@ describe('exporter Model', function () {
         name: 'dummy'
       }
 
-      var params = exporter.getOgrParams(format, inFile, outFile, null, options).split(' ')
-      params[6].should.equal(outFile)
-      params[7].should.equal(inFile)
-      done()
+      exporter.getOgrParams(format, inFile, outFile, null, options, function (err, cmd) {
+        should.not.exist(err)
+        var params = cmd.split(' ')
+        params[6].should.equal(outFile)
+        params[7].should.equal(inFile)
+        done()
+      })
     })
 
     it('should support WKID as an option for shapefiles', function (done) {
@@ -99,10 +102,13 @@ describe('exporter Model', function () {
         wkid: 102101
       }
 
-      var params = exporter.getOgrParams(format, inFile, outFile, null, options).split(' ')
-      params[7].should.equal(outFile)
-      params[8].should.equal(inFile)
-      done()
+      exporter.getOgrParams(format, inFile, outFile, null, options, function (err, cmd) {
+        should.not.exist(err)
+        var params = cmd.split(' ')
+        params[7].should.equal(outFile)
+        params[8].should.equal(inFile)
+        done()
+      })
     })
 
     it('should create a correct ogr string of commands with a WKID', function (done) {
@@ -114,12 +120,15 @@ describe('exporter Model', function () {
         name: 'dummy',
         outSR: 2962
       }
-      var params = exporter.getOgrParams(format, inFile, outFile, null, options).split(' ')
-      params[10].should.equal('\'PROJCS["NAD_1983_CSRS_UTM_Zone_21N",GEOGCS["GCS_North_American_1983_CSRS",DATUM["D_North_American_1983_CSRS",SPHEROID["GRS_1980",6378137.0,298.257222101],TOWGS84[-0.9956,1.9013,0.5215,0.025915,0.009426,0.011599,-0.00062]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-57.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]\'')
-      // make sure the format is "Esri Shapefile"
-      var outFormat = params[5] + ' ' + params[6]
-      outFormat.should.equal('"ESRI Shapefile"')
-      done()
+      exporter.getOgrParams(format, inFile, outFile, null, options, function (err, cmd) {
+        should.not.exist(err)
+        var params = cmd.split(' ')
+        params[10].should.equal('\'PROJCS["NAD_1983_CSRS_UTM_Zone_21N",GEOGCS["GCS_North_American_1983_CSRS",DATUM["D_North_American_1983_CSRS",SPHEROID["GRS_1980",6378137.0,298.257222101],TOWGS84[-0.9956,1.9013,0.5215,0.025915,0.009426,0.011599,-0.00062]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-57.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]\'')
+        // make sure the format is "Esri Shapefile"
+        var outFormat = params[5] + ' ' + params[6]
+        outFormat.should.equal('"ESRI Shapefile"')
+        done()
+      })
     })
 
     it('should create a correct ogr string of commands with a latest WKID', function (done) {
@@ -131,12 +140,15 @@ describe('exporter Model', function () {
         name: 'dummy',
         outSR: {wkid: 102646, latestWkid: 2230}
       }
-      var params = exporter.getOgrParams(format, inFile, outFile, null, options).split(' ')
-      params[10].should.equal('\'PROJCS["NAD_1983_StatePlane_California_VI_FIPS_0406_Feet",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["False_Easting",6561666.666666666],PARAMETER["False_Northing",1640416.666666667],PARAMETER["Central_Meridian",-116.25],PARAMETER["Standard_Parallel_1",32.78333333333333],PARAMETER["Standard_Parallel_2",33.88333333333333],PARAMETER["Latitude_Of_Origin",32.16666666666666],UNIT["Foot_US",0.3048006096012192]]\'')
-      // make sure the format is "Esri Shapefile"
-      var outFormat = params[5] + ' ' + params[6]
-      outFormat.should.equal('"ESRI Shapefile"')
-      done()
+      exporter.getOgrParams(format, inFile, outFile, null, options, function (err, cmd) {
+        should.not.exist(err)
+        var params = cmd.split(' ')
+        params[10].should.equal('\'PROJCS["NAD_1983_StatePlane_California_VI_FIPS_0406_Feet",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["False_Easting",6561666.666666666],PARAMETER["False_Northing",1640416.666666667],PARAMETER["Central_Meridian",-116.25],PARAMETER["Standard_Parallel_1",32.78333333333333],PARAMETER["Standard_Parallel_2",33.88333333333333],PARAMETER["Latitude_Of_Origin",32.16666666666666],UNIT["Foot_US",0.3048006096012192]]\'')
+        // make sure the format is "Esri Shapefile"
+        var outFormat = params[5] + ' ' + params[6]
+        outFormat.should.equal('"ESRI Shapefile"')
+        done()
+      })
     })
 
     it('should apply a datum transformation on 2927', function (done) {
