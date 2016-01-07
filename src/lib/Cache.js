d549b8fa77d7c0d67936561dbafb0dedb856b7be
dmfenton <dfenton@esri.com>
2016-01-07 16:17:21 -0500

    remove timers from koop.cache

--- src/lib/Cache.js
+++ src/lib/Cache.js
@@ -16,12 +16,8 @@ function Cache () {
    * @param {function} callback - the callback to return errors and data to
    */
   this.insert = function (type, key, data, layerId, callback) {
-    var self = this
     this.db.insert(type + ':' + key, data, layerId, function (err, success) {
-      self.timerSet(type + ':' + key + ':timer', 3600000, function (error, timer) {
-        if (error) return callback(error)
-        callback(err, success)
-      })
+      callback(err, success)
     })
   }
 
@@ -38,66 +34,6 @@ function Cache () {
     this.db.insertPartial(type + ':' + key, data, layerId, function (err, success) {
       callback(err, success)
     })
-  }
-
-  /**
-   * Handles the response from the DB select for feature data
-   * if a cache timer has expired we ask if the data in the 3rd party API has changed
-   * if it has changed then each Model's checkthis function will return the NEW data
-   * if it hasnt changed then checkthis will return false and the old data will be sent back
-   *
-   * @param {string} type - the provider type, used for setting provider based timers etc
-   * @param {string} key - a provider based id for a dataset (defined by the provider)
-   * @param {object} data - valid geojson
-   * @param {object} options - optional query params like where, geometry
-   * @param {function} callback - the callback to return errors and data to
-   */
-  this.process = function (type, key, data, options, callback) {
-    var self = this
-    // timing at which we'll check the validity of the cache
-    var checkTime = (60 * 60 * 1000) // 60 mins
-
-    if (!data.length) {
-      callback('Not found', null)
-    } else {
-      // checks the timer
-      var timerKey = [type, key, (options.layer || 0), 'timer'].join(':')
-
-      this.db.timerGet(timerKey, function (error, timer) {
-        if (error) return callback(error)
-
-        // got a timer, therefore we are good and just return
-        if (timer) return callback(null, data)
-
-        // expired, hit the API to check the latest sha
-        if (global[type] && global[type].checkCache) {
-          global[type].checkCache(key, data, options, function (err, success) {
-            if (err) return callback(err)
-
-            if (!success) {
-              // false is good -> reset timer and return data
-              // cache returned true, return current data
-              self.db.timerSet(timerKey, checkTime, function (error, timer) {
-                if (error) return callback(error)
-                callback(null, data)
-              })
-            } else {
-              // we need to remove and save new data
-              self.remove(type, key, options, function () {
-                self.insert(type, key, data, (options.layer || 0), function (err, res) {
-                  self.db.timerSet(timerKey, checkTime, function (error, timer) {
-                    if (error) return callback(error)
-                    callback(err, success)
-                  })
-                })
-              })
-            }
-          })
-        } else {
-          callback(null, data)
-        }
-      })
-    }
   }
 
   /**
@@ -126,12 +62,10 @@ function Cache () {
    * @param {function} callback - the callback to return errors and data to
    */
   this.get = function (type, key, options, callback) {
-    var self = this
     var table = type + ':' + key
     var query = this.decodeGeoservices(options)
-    this.db.select(table, query, function (err, result) {
-      if (err) return callback(err)
-      self.process(type, key, result, query, callback)
+    this.db.select(table, query, function (err, data) {
+      callback(err, data)
     })
   }
 
@@ -270,31 +204,6 @@ function Cache () {
    */
   this.serviceRemove = function (type, id, callback) {
     this.db.serviceRemove(type, id, callback)
-  }
-
-  /**
-   * Set a timer in the DB for a table
-   *
-   * Timers are used as a way to prevent providers from checking for data
-   * changes in APIs on every request. Once data are cached a timer of an 60 mins
-   * is used to prevent the provider from asking the API is the data have changed.
-   *
-   * @param {string} table - the table to create a timer for
-   * @param {number} length - the length of the timer in milliseconds
-   * @param {function} callback - the callback to return any errors
-   */
-  this.timerSet = function (table, length, callback) {
-    this.db.timerSet(table, length, callback)
-  }
-
-  /**
-   * Gets a timer for a table
-   *
-   * @param {string} table - the table for which to get any timer
-   * @param {function} callback - the callback to return any errors
-   */
-  this.timerGet = function (table, callback) {
-    this.db.timerGet(table, callback)
   }
 
 	/**
