2f8290193fcd8381e9bc6cb5cd4bc33800714069
dmfenton <dfenton@esri.com>
2016-03-09 11:26:26 -0800

    streamline base controller

--- src/lib/BaseController.js
+++ src/lib/BaseController.js
@@ -1,4 +1,4 @@
-var featureServices = require('./FeatureServices')
+const featureServices = require('./FeatureServices')
 
 /**
  * A base controller that we can use as a prototype
@@ -11,96 +11,64 @@ function Controller () {
    *
    * @param  {Object}   req
    * @param  {Object}   res
-   * @param  {Object}   err
-   * @param  {Object}   data
-   * @param  {Function} callback
+   * @param  {Object}   geojson
    */
-  function processFeatureServer (req, res, err, data, callback) {
+  function processFeatureServer (req, res, geojson) {
     delete req.query.geometry
 
-    if (err) return res.status(500).json(err)
-    if (!data) return res.status(400).send('There a problem accessing this repo')
+    if (!geojson) return res.status(400).send('There a problem accessing this repo')
 
     // check for info requests and respond like ArcGIS Server would
-    if (req._parsedUrl.pathname.substr(-4) === 'info') {
-      var arcGisServerLikeResponse = {
-        currentVersion: 10.21,
-        fullVersion: '10.2.1',
-        soapUrl: 'http://sampleserver6.arcgisonline.com/arcgis/services',
-        secureSoapUrl: 'https://sampleserver6.arcgisonline.com/arcgis/services',
-        authInfo: {
-          isTokenBasedSecurity: true,
-          tokenServicesUrl: 'https://sampleserver6.arcgisonline.com/arcgis/tokens/',
-          shortLivedTokenValidity: 60
-        }
-      }
-
-      return res.status(200).send(arcGisServerLikeResponse)
-    }
-
-    if (featureServices[req.params.layer]) {
-      // requests for specific layers - pass data and the query string
-      featureServices[req.params.layer](data, req.query || {}, function (err, d) {
-        if (err) {
-          res.status(400).send(err)
-          return
-        }
-        // limit response to 1000
-        if (d.feature && d.features.length > 1000) {
-          d.features = d.features.splice(0, 1000)
-        }
-        if (callback) {
-          res.send(callback + '(' + JSON.stringify(d) + ')')
-        } else {
-          res.json(d)
-        }
-      })
-    } else {
-      // have a layer
-      if (req.params.layer && data[req.params.layer]) {
-        // pull out the layer data
-        data = data[req.params.layer]
-      } else if (req.params.layer && !data[req.params.layer]) {
-        res.status(404).send('Layer not found')
-      }
-      if (req.params.method && featureServices[req.params.method]) {
-        // we have a method call like "/layers"
-        featureServices[req.params.method](data, req.query || {}, function (err, d) {
-          // limit response to 1000
-          if (err) {
-            res.status(400).send(err)
-            return
-          }
-          if (d.features && d.features.length > 1000) {
-            d.features = d.features.splice(0, 1000)
-          }
-          if (callback) {
-            res.send(callback + '(' + JSON.stringify(d) + ')')
-          } else {
-            res.json(d)
-          }
-        })
-      } else {
-        // make a straight up feature service info request
-        // we still pass the layer here to conform to info method, though its undefined
-        featureServices.info(data, req.params.layer, req.query, function (err, d) {
-          if (err) {
-            if (callback) callback(err)
-            else res.status(500).send(err)
-          }
-          if (callback) {
-            res.send(callback + '(' + JSON.stringify(d) + ')')
-          } else {
-            res.json(d)
-          }
-        })
-      }
-    }
+    if (isInfoReq(req)) return res.status(200).send(serverInfoResponse)
+    // if this is for a method we can handle like query
+    const serverMethod = getServerMethod(req.params)
+    if (serverMethod) return execServerMethod(serverMethod, req, res, geojson)
+    // make a straight up feature service info request
+    // we still pass the layer here to conform to info method, though its undefined
+    else return execInfo(geojson, req.params.layer, req.query, res)
   }
 
-  return {
-    processFeatureServer: processFeatureServer
+  return {processFeatureServer}
+}
+
+const serverInfoResponse = {
+  currentVersion: 10.21,
+  fullVersion: '10.2.1',
+  soapUrl: 'http://sampleserver6.arcgisonline.com/arcgis/services',
+  secureSoapUrl: 'https://sampleserver6.arcgisonline.com/arcgis/services',
+  authInfo: {
+    isTokenBasedSecurity: true,
+    tokenServicesUrl: 'https://sampleserver6.arcgisonline.com/arcgis/tokens/',
+    shortLivedTokenValidity: 60
   }
+}
+
+function isInfoReq (req) {
+  req._parsedUrl.pathname.substr(-4) === 'info'
+}
+
+function getServerMethod (params) {
+  if (params.method && featureServices[params.method]) return featureServices[params.method]
+  else return false
+}
+
+function execServerMethod (method, req, res, geojson) {
+  // we have a method call like "/layers"
+  method(geojson, req.query || {}, function (err, d) {
+    // limit response to 1000
+    if (err) return res.status(400).send(err)
+    if (d.features && d.features.length > 1000) d.features = d.features.splice(0, 1000)
+    if (req.query.callback) return res.send(req.query.callback + '(' + JSON.stringify(d) + ')')
+    res.status(200).json(d)
+  })
+}
+
+function execInfo (geojson, layer, query, res) {
+  featureServices.info(geojson, layer, query, function (err, d) {
+    if (err) return res.status(500).send(err)
+    if (query.callback) return res.send(query.callback + '(' + JSON.stringify(d) + ')')
+    res.status(200).json(d)
+  })
 }
 
 module.exports = Controller
