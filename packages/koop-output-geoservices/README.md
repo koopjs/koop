fda39bbbd9a60f32480ccaf578ac59e0bb3d9bc7
dmfenton <dfenton@esri.com>
2017-01-25 13:51:09 -0500

    initial commit

---
+++ packages/koop-output-geoservices/README.md
@@ -0,0 +1,45 @@
+# Koop FeatureServer Plugin
+
+Wraps [FeatureServer](https://github.com/featureserver/featureserver) into a [Koop](http://koopjs.github.io) Output plugin.
+
+## Usage
+```js
+const Koop = require('koop')
+const config = require('config')
+const koop = new Koop(config)
+const FeatureServer = require('koop-featureserver-plugin')
+const Provider = require('koop-agol') // any koop provider here
+
+// All output plugins must be registered before any providers are registered
+koop.register(FeatureServer)
+koop.register(Provider)
+
+koop.server.listen(80)
+```
+
+## Routes
+
+```js
+[
+  {
+    path: 'FeatureServer/:layer/:method',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer/layers',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer/:layer',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  }
+]
+```
---
+++ packages/koop-output-geoservices/index.js
@@ -0,0 +1,42 @@
+var FeatureServer = require('featureserver')
+var express = require('express')
+
+function Plugin () {}
+
+Plugin.prototype.featureServer = function (req, res) {
+  // model will be available when this is instantiated with the Koop controller
+  this.model.getData(req, function (err, data) {
+    if (err) res.status(500).json({error: err.message})
+    else FeatureServer.route(req, res, data)
+  })
+}
+
+Plugin.routes = [
+  {
+    path: 'FeatureServer/:layer/:method',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer/layers',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer/:layer',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  },
+  {
+    path: 'FeatureServer',
+    methods: ['get', 'post'],
+    handler: 'featureServer'
+  }
+]
+
+Plugin.dependencies = 'cache'
+Plugin.plugin_name = 'FeatureServer'
+Plugin.type = 'output'
+Plugin.version = require('./package.json').version
+
+module.exports = Plugin
