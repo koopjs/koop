055f05cdebfc91da6a8a4860cedbf0304e1295ef
Daniel Fenton <dfenton@esri.com>
2017-08-14 20:47:22 -0400

    Basic WFS Support (#34)
    * add basic WFS support
    
    * work with srsName or srsname
    
    * kick travis
    
    * update package.json
    
    * add test for wfs projection

--- packages/winnow/test/wfs.js
+++ packages/winnow/test/wfs.js
@@ -1,5 +1,6 @@
 const test = require('tape')
 const run = require('./helper').run
+const winnow = require('../src')
 
 test('WFS with a bbox in Web Mercator with geojson output', t => {
   const options = {
@@ -8,4 +9,19 @@ test('WFS with a bbox in Web Mercator with geojson output', t => {
     srsName: 'EPSG:3857'
   }
   run('restaurants', options, 249, t)
+})
+
+test('WFS with a bbox in Web Mercator with geojson output', t => {
+  t.plan(2)
+  const options = {
+    outputFormat: 'application/json',
+    bbox: '-12993071.816030473,3913575.8482084945,-12836528.782102507,4070118.8821364585,EPSG:3857',
+    srsName: 'EPSG:3857',
+    count: 1
+  }
+  const features = require('./fixtures/restaurants.json')
+  const filtered = winnow.query(features, options)
+  const coords = filtered.features[0].geometry.coordinates
+  t.equal(coords[0], -12978087.878608154, 'projected correctly')
+  t.equal(filtered.features.length, 1, 'limited correctly')
 })
