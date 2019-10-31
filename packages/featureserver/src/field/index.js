9edb64596a91ee694370b1b021ac70385c2f50e3
ahamilt <alan_hamilton@trimble.com>
2019-10-31 13:38:36 +0000

    Fix moving object id field to start. (#167)
    * Fix moving object id field to start.

--- packages/featureserver/src/field/index.js
+++ packages/featureserver/src/field/index.js
@@ -51,7 +51,10 @@ function computeFieldsCollection (data, requestContext, options = {}) {
     })
   })
   // Ensure the OBJECTID field is first in the array
-  responsefields.unshift(responsefields.splice(responsefields.findIndex(field => field.name === 'OBJECTID'), 1)[0])
+  const idFieldIndex = responsefields.findIndex(field => field.type === templates.objectIDField.type)
+  if (idFieldIndex !== -1) {
+    responsefields.unshift(responsefields.splice(idFieldIndex, 1)[0])
+  }
 
   return responsefields
 }
