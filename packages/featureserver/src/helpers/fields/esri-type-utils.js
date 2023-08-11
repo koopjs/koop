246e7a0dbad596a3ebe186a2eed9896e8520d841
Rich Gwozdz <rgwozdz@esri.com>
2023-08-11 09:29:06 -0700

    chore: fix lint

--- packages/featureserver/src/helpers/fields/esri-type-utils.js
+++ packages/featureserver/src/helpers/fields/esri-type-utils.js
@@ -2,40 +2,40 @@ const { getDataTypeFromValue } = require('../data-type-utils');
 const {
   ESRI_FIELD_TYPE_STRING,
   ESRI_FIELD_TYPE_DATE,
-  ESRI_FIELD_TYPE_DOUBLE
+  ESRI_FIELD_TYPE_DOUBLE,
 } = require('./constants');
 
-function getEsriTypeFromDefinition (typeDefinition = '') {
+function getEsriTypeFromDefinition(typeDefinition = '') {
   switch (typeDefinition.toLowerCase()) {
-  case 'double':
-    return ESRI_FIELD_TYPE_DOUBLE;
-  case 'integer':
-    return 'esriFieldTypeInteger';
-  case 'date':
-    return ESRI_FIELD_TYPE_DATE;
-  case 'blob':
-    return 'esriFieldTypeBlob';
-  case 'geometry':
-    return 'esriFieldTypeGeometry';
-  case 'globalid':
-    return 'esriFieldTypeGlobalID';
-  case 'guid':
-    return 'esriFieldTypeGUID';
-  case 'raster':
-    return 'esriFieldTypeRaster';
-  case 'single':
-    return 'esriFieldTypeSingle';
-  case 'smallinteger':
-    return 'esriFieldTypeSmallInteger';
-  case 'xml':
-    return 'esriFieldTypeXML';
-  case 'string':
-  default:
-    return ESRI_FIELD_TYPE_STRING;
+    case 'double':
+      return ESRI_FIELD_TYPE_DOUBLE;
+    case 'integer':
+      return 'esriFieldTypeInteger';
+    case 'date':
+      return ESRI_FIELD_TYPE_DATE;
+    case 'blob':
+      return 'esriFieldTypeBlob';
+    case 'geometry':
+      return 'esriFieldTypeGeometry';
+    case 'globalid':
+      return 'esriFieldTypeGlobalID';
+    case 'guid':
+      return 'esriFieldTypeGUID';
+    case 'raster':
+      return 'esriFieldTypeRaster';
+    case 'single':
+      return 'esriFieldTypeSingle';
+    case 'smallinteger':
+      return 'esriFieldTypeSmallInteger';
+    case 'xml':
+      return 'esriFieldTypeXML';
+    case 'string':
+    default:
+      return ESRI_FIELD_TYPE_STRING;
   }
 }
 
-function getEsriTypeFromValue (value) {
+function getEsriTypeFromValue(value) {
   const dataType = getDataTypeFromValue(value);
 
   return getEsriTypeFromDefinition(dataType);
@@ -43,5 +43,5 @@ function getEsriTypeFromValue (value) {
 
 module.exports = {
   getEsriTypeFromDefinition,
-  getEsriTypeFromValue
+  getEsriTypeFromValue,
 };
