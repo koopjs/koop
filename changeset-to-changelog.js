cfcb4ae102545b867daec90254baa4fff33588ca
Rich Gwozdz <rgwozdz@esri.com>
2023-08-15 06:25:06 -0700

    chore: clean package files

--- changeset-to-changelog.js
+++
@@ -1,8 +0,0 @@
-const { default: { getDependencyReleaseLine } } = require('@changesets/changelog-git');
-
-const getReleaseLine = async (changeset) => {
-  return changeset.summary;
-};
-
-
-module.exports = { getDependencyReleaseLine, getReleaseLine };
