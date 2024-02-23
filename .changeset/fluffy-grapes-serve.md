ff596ff29421dabc2edf79a1dbc84e8cf424aaa3
Rich Gwozdz <rgwozdz@esri.com>
2024-02-23 13:16:47 -0800

    feat: update hash function

---
+++ .changeset/fluffy-grapes-serve.md
@@ -0,0 +1,5 @@
+---
+"@koopjs/winnow": minor
+---
+
+- swap stringhash for murmurhash
--- packages/winnow/src/filter-and-transform/helpers/hash-function.js
+++ packages/winnow/src/filter-and-transform/helpers/hash-function.js
@@ -1,17 +1,19 @@
 const hashFixture = require('./hash-fixture');
 const USE_JAVASCRIPT_HASHING = process.env.OBJECTID_FEATURE_HASH === 'javascript';
-const stringHash = require('string-hash');
+const murmurhash = require('murmurhash');
 
 function getHashFunction () {
-  if (USE_JAVASCRIPT_HASHING) return stringHash;
+  if (USE_JAVASCRIPT_HASHING) {
+    return murmurhash;
+  }
 
-  // Try to use farmhash, fallback to JavaScript only hashing library
+  // Try to use farmhash, fallback to native JavaScript hashing library
   try {
     const hashFunction = require('farmhash').hash32;
     hashFunction(JSON.stringify(hashFixture));
     return hashFunction;
   } catch (e) {
-    return stringHash;
+    return murmurhash;
   }
 }
 
