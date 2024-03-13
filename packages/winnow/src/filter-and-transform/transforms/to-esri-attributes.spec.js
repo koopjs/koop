3ca771401b3879321488d3b9a7d2912cfe4d265c
Rich Gwozdz <rgwozdz@esri.com>
2024-03-13 09:18:04 -0700

    fix: separate idField debug messages (#949)
    * fix: separate idField debug messages
    
    * chore: update changeset

--- packages/winnow/src/filter-and-transform/transforms/to-esri-attributes.spec.js
+++ packages/winnow/src/filter-and-transform/transforms/to-esri-attributes.spec.js
@@ -3,100 +3,188 @@ const proxyquire = require('proxyquire');
 const modulePath = './to-esri-attributes';
 const toEsriAttributes = require(modulePath);
 
-test('toEsriAttributes, does not require idField', t => {
+test('toEsriAttributes, does not require idField', (t) => {
   // (properties, geometry, dateFields, requiresObjectId, idField)
-  const result = toEsriAttributes({ foo: 'bar' }, { coordinates: [-118, 34] }, '', 'false', '');
+  const result = toEsriAttributes(
+    { foo: 'bar' },
+    { coordinates: [-118, 34] },
+    '',
+    'false',
+    '',
+  );
   t.deepEquals(result, { foo: 'bar' });
   t.end();
 });
 
-test('toEsriAttributes, requires idField and properties contain it', t => {
+test('toEsriAttributes, requires idField and properties contain it', (t) => {
   // (properties, geometry, dateFields, requiresObjectId, idField)
-  const result = toEsriAttributes({ foo: 'bar', OBJECTID: 11111 }, { coordinates: [-118, 34] }, '', 'true', 'OBJECTID');
+  const result = toEsriAttributes(
+    { foo: 'bar', OBJECTID: 11111 },
+    { coordinates: [-118, 34] },
+    '',
+    'true',
+    'OBJECTID',
+  );
   t.deepEquals(result, { foo: 'bar', OBJECTID: 11111 });
   t.end();
 });
 
-test('toEsriAttributes, requires idField, properties contain it, but logs debug message', t => {
+test('toEsriAttributes, requires idField, properties contain it, but logs debug string message', (t) => {
   const toEsriAttributes = proxyquire(modulePath, {
     '../../log-manager': {
       logger: {
         debug: (message) => {
-          t.equals(message, `Unique-identifier (\`OBJECTID\`) has a value (abc) that is not a valid integer (0 to ${Number.MAX_SAFE_INTEGER}); this may cause problems in some clients.`);
-        }
-      }
-    }
+          t.equals(
+            message,
+            'Unique-identifier ("OBJECTID") has a value (abc) that is not an integer-type, it is a string; this may cause problems in some clients.',
+          );
+        },
+      },
+    },
   });
   // (properties, geometry, dateFields, requiresObjectId, idField)
-  const result = toEsriAttributes({ foo: 'bar', OBJECTID: 'abc' }, { coordinates: [-118, 34] }, '', 'true', 'OBJECTID');
+  const result = toEsriAttributes(
+    { foo: 'bar', OBJECTID: 'abc' },
+    { coordinates: [-118, 34] },
+    '',
+    'true',
+    'OBJECTID',
+  );
   t.deepEquals(result, { foo: 'bar', OBJECTID: 'abc' });
   t.end();
 });
 
-test('toEsriAttributes, requires idField and properties do not have one, so create hash', t => {
+test('toEsriAttributes, requires idField, properties contain it, but logs debug range message', (t) => {
+  const toEsriAttributes = proxyquire(modulePath, {
+    '../../log-manager': {
+      logger: {
+        debug: (message) => {
+          t.equals(
+            message,
+            'Unique-identifier ("OBJECTID") has a value (-1) that is not a valid integer range (0 to 9007199254740991); this may cause problems in some clients.',
+          );
+        },
+      },
+    },
+  });
+  // (properties, geometry, dateFields, requiresObjectId, idField)
+  const result = toEsriAttributes(
+    { foo: 'bar', OBJECTID: -1 },
+    { coordinates: [-118, 34] },
+    '',
+    'true',
+    'OBJECTID',
+  );
+  t.deepEquals(result, { foo: 'bar', OBJECTID: -1 });
+  t.end();
+});
+
+test('toEsriAttributes, requires idField and properties do not have one, so create hash', (t) => {
   const toEsriAttributes = proxyquire(modulePath, {
     '../helpers': {
       createIntegerHash: (string) => {
-        t.deepEquals(string, '{"properties":{"foo":"bar"},"geometry":{"coordinates":[-118,34]}}');
+        t.deepEquals(
+          string,
+          '{"properties":{"foo":"bar"},"geometry":{"coordinates":[-118,34]}}',
+        );
         return 99999;
-      }
-    }
+      },
+    },
   });
-  
+
   // (properties, geometry, dateFields, requiresObjectId, idField)
-  const result = toEsriAttributes({ foo: 'bar' }, { coordinates: [-118, 34] }, '', 'true', 'null');
+  const result = toEsriAttributes(
+    { foo: 'bar' },
+    { coordinates: [-118, 34] },
+    '',
+    'true',
+    'null',
+  );
   t.deepEquals(result, { foo: 'bar', OBJECTID: 99999 });
   t.end();
 });
 
-test('toEsriAttributes, does not require idField, has dateFields', t => {
+test('toEsriAttributes, does not require idField, has dateFields', (t) => {
   // (properties, geometry, dateFields, requiresObjectId, idField)
   const date1 = new Date();
   const date2 = new Date(1000);
-  const result = toEsriAttributes({ hello: 'world', foo: date1.toISOString(), bar: date2.toISOString() }, { coordinates: [-118, 34] }, 'foo,bar', 'false', '');
-  t.deepEquals(result, { hello: 'world', foo: date1.getTime(), bar: date2.getTime() });
+  const result = toEsriAttributes(
+    { hello: 'world', foo: date1.toISOString(), bar: date2.toISOString() },
+    { coordinates: [-118, 34] },
+    'foo,bar',
+    'false',
+    '',
+  );
+  t.deepEquals(result, {
+    hello: 'world',
+    foo: date1.getTime(),
+    bar: date2.getTime(),
+  });
   t.end();
 });
 
-test('toEsriAttributes, require idField, has dateFields', t => {
+test('toEsriAttributes, require idField, has dateFields', (t) => {
   const date1 = new Date();
   const date2 = new Date(1000);
-  
+
   const toEsriAttributes = proxyquire(modulePath, {
     '../helpers': {
       createIntegerHash: (string) => {
-        t.deepEquals(string, `{"properties":{"hello":"world","foo":"${date1.toISOString()}","bar":"${date2.toISOString()}"},"geometry":{"coordinates":[-118,34]}}`);
+        t.deepEquals(
+          string,
+          `{"properties":{"hello":"world","foo":"${date1.toISOString()}","bar":"${date2.toISOString()}"},"geometry":{"coordinates":[-118,34]}}`,
+        );
         return 99999;
-      }
-    }
+      },
+    },
   });
-  
+
   // (properties, geometry, dateFields, requiresObjectId, idField)
-  const result = toEsriAttributes({ hello: 'world', foo: date1.toISOString(), bar: date2.toISOString() }, { coordinates: [-118, 34] }, 'foo,bar', 'true', 'null');
-  t.deepEquals(result, { OBJECTID: 99999, hello: 'world', foo: date1.getTime(), bar: date2.getTime() });
+  const result = toEsriAttributes(
+    { hello: 'world', foo: date1.toISOString(), bar: date2.toISOString() },
+    { coordinates: [-118, 34] },
+    'foo,bar',
+    'true',
+    'null',
+  );
+  t.deepEquals(result, {
+    OBJECTID: 99999,
+    hello: 'world',
+    foo: date1.getTime(),
+    bar: date2.getTime(),
+  });
   t.end();
 });
 
-test('toEsriAttributes, does not require idField, has null dateField', t => {
+test('toEsriAttributes, does not require idField, has null dateField', (t) => {
   // (properties, geometry, dateFields, requiresObjectId, idField)
   const date2 = new Date(1000);
-  const result = toEsriAttributes({ hello: 'world', foo: null, bar: date2.toISOString() }, { coordinates: [-118, 34] }, 'foo,bar', 'false', '');
+  const result = toEsriAttributes(
+    { hello: 'world', foo: null, bar: date2.toISOString() },
+    { coordinates: [-118, 34] },
+    'foo,bar',
+    'false',
+    '',
+  );
   t.deepEquals(result, { hello: 'world', foo: null, bar: date2.getTime() });
   t.end();
 });
 
-test('toEsriAttributes, does not require idField, has nested properties', t => {
-  const result = toEsriAttributes({
-    hello: 'world',
-    nested: {
-      foo: 'bar'
-    }
-  }, {
-    coordinates: [-118, 34]
-  },
-  '',
-  'false',
-  '');
+test('toEsriAttributes, does not require idField, has nested properties', (t) => {
+  const result = toEsriAttributes(
+    {
+      hello: 'world',
+      nested: {
+        foo: 'bar',
+      },
+    },
+    {
+      coordinates: [-118, 34],
+    },
+    '',
+    'false',
+    '',
+  );
   t.deepEquals(result, { hello: 'world', nested: '{"foo":"bar"}' });
   t.end();
 });
